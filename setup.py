a21fafc87a47b94c02217feecc82f7a9fc17c58a
Ryszard Szopa <ryszard.szopa@gmail.com>
2011-01-14 14:03:45 +0100

    Adding setup.py.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='blog',
+      version='0.1',
+      description='Small blog app for Django 1.0.2',
+      author='Ryszard Szopa',
+      author_email='ryszard.szopa@gmail.com',
+      url='https://github.com/ryszard/django-comment-utils/',
+      packages=['blog'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: Poetic License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'],
+      )
